ab28107503e08f29cafcc0b9f923e9f0e2eeda62
aanper <mail@s3f.ru>
2021-04-24 19:34:47 +0300

    choose map

--- src/game.js
+++ src/game.js
@@ -1,15 +1,61 @@
 
 function game_update(t, dt, state) {
-    state.camera.position.z += -(state.forward - state.backward) * dt * 2;
     state.camera.position.x += (state.right - state.left) * dt * 2;
     state.camera.position.y += (state.up - state.down) * dt * 2;
+    state.camera.position.z += -(state.forward - state.backward) * dt * 2;
+
+    // console.log("lookat", state.camera.lookAt);
+
+    // state.ambient.intensity = 1 + 1 * Math.sin(t * 4);
+
+    // state.panorama[0].visible = (0.5 + Math.sin(t * 200) * 0.5) > 0.5;
+    // state.panorama[1].visible = (1 - (0.5 + Math.sin(t * 200) * 0.5)) > 0.5;
+
+    let min_distance = 1e308;
+    let min_id = null;
+
+    state.panorama.forEach((v, id) => {
+        let distance =
+            Math.pow(v.position.x - state.camera.position.x, 2) +
+            Math.pow(v.position.y - state.camera.position.y, 2) +
+            Math.pow(v.position.z - state.camera.position.z, 2);
+        if(distance < min_distance) {
+            min_distance = distance;
+            min_id = id;
+        }
+        v.visible = false;
+    });
+
+    state.panorama[min_id].visible = true;
+
+    /*
+    if(state.correction) {
+        state.movable_panorama.position.copy(state.camera.position);
+
+        state.movable_panorama.rotation.x = state.camera.rotation.x;
+        state.movable_panorama.rotation.y = state.camera.rotation.y;// + Math.PI;
+        state.movable_panorama.rotation.z = state.camera.rotation.z;
+        
+
+        state.movable_panorama.rotation.copy(state.camera.rotation);
+        state.movable_panorama.updateMatrix();
+        // state.movable_panorama.translateZ( - 10 );
+
+        
+        /*
+        state.movable_panorama.position.x = state.camera.position.x;
+        state.movable_panorama.position.y = state.camera.position.y;
+        state.movable_panorama.position.z = state.camera.position.z;
+        *
+    }
+    */
 }
 
 function game_init() {
     let state = {};
     
     state.scene = new THREE.Scene();
-    state.scene.background = new THREE.Color('purple');
+    state.scene.background = new THREE.Color('black');
 
     state.camera = new THREE.PerspectiveCamera(
         80, window.innerWidth / window.innerHeight, 0.1, 1000
@@ -23,13 +69,12 @@ function game_init() {
     light.color.set('white');
     light.position.set(3, 1, 5);
     state.scene.add(light);
-    
 
     
     const ambient = new THREE.AmbientLight(0x010101, 0.4);
     ambient.color.set('white');
     state.scene.add(ambient);
-    
+    state.ambient = ambient;
     
     const red_material = new THREE.MeshLambertMaterial({color: 0xff0000});
 
@@ -50,47 +95,39 @@ function game_init() {
             red_material
         );
         sphere_1.position.x = 0;
+        sphere_1.position.y = 0;
         sphere_1.position.z = -5;
         state.scene.add(sphere_1);
     }
+    
+    const textureLoader = new THREE.TextureLoader();
 
-    {
-        const green_material = new THREE.MeshLambertMaterial({
-            color: 0x00ff00,
-            side: THREE.DoubleSide
+    state.panorama = [];
+
+    // fixed panorama
+    SELENGA_MAP.forEach(map_tex => {
+        let texture = textureLoader.load(map_tex.name);
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        texture.encoding = THREE.sRGBEncoding;
+
+        const geometry = new THREE.SphereGeometry(1, 100, 100, 0, Math.PI);
+        const material = new THREE.MeshLambertMaterial({
+            map: texture,
+            side: THREE.DoubleSide,
+            opacity: 0.99,
+            transparent: true
         });
-        
-        const outer_sphere = new THREE.Mesh(
-            new THREE.SphereGeometry(6, 32, 32),
-            green_material
-        );
-        // sphere.rotation.y = 0.4;
-        outer_sphere.position.x = 0;
-        // scene.add(outer_sphere);
-    }
-
-    {
-        const textureLoader = new THREE.TextureLoader();
-        textureEquirec = textureLoader.load('assets/inside23.png');
-
-        textureEquirec.mapping = THREE.EquirectangularReflectionMapping;
-        textureEquirec.encoding = THREE.sRGBEncoding;
-
-        const macht_geometry = new THREE.SphereGeometry(1, 32, 32, 0, Math.PI);
-        const macht_material = new THREE.MeshLambertMaterial({
-            map: textureEquirec,
-            side: THREE.DoubleSide
-        });
-        const macht_sphere = new THREE.Mesh(macht_geometry, macht_material);
-        macht_sphere.rotation.y = Math.PI;
-        macht_sphere.rotation.x = 0;
-        // macht_sphere.position.set(3, 3.3, -10);
-        let ssize = 8;
-        macht_sphere.scale.set(ssize, ssize, ssize);
-        state.macht_sphere = macht_sphere;
-
-        state.scene.add(macht_sphere);
-    }
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = map_tex.position[0];
+        mesh.position.y = map_tex.position[1];
+        mesh.position.z = map_tex.position[2];
+        mesh.rotation.x = map_tex.rotation[0];
+        mesh.rotation.y = map_tex.rotation[1];
+        mesh.rotation.z = map_tex.rotation[2];
+        mesh.scale.set(1, 1, -1);
+        state.scene.add(mesh);
+        state.panorama.push(mesh);
+    });
 
     state.up = 0;
     state.down = 0;
@@ -98,6 +135,8 @@ function game_init() {
     state.right = 0;
     state.forward = 0;
     state.backward = 0;
+
+    state.correction = false;
 
     return state;
 }
@@ -120,5 +159,15 @@ function game_handle_key(key, is_press, state) {
     }
     if(key === "q") {
         state.down = is_press ? 1 : 0;
+    }
+
+    if(key == "z" && is_press) {
+        console.log(`
+            mesh.position.x = ${state.movable_panorama.position.x};
+            mesh.position.y = ${state.movable_panorama.position.y};
+            mesh.position.z = ${state.movable_panorama.position.z};
+            mesh.rotation.x = ${state.movable_panorama.rotation.x};
+            mesh.rotation.y = ${state.movable_panorama.rotation.y};
+            mesh.rotation.z = ${state.movable_panorama.rotation.z};`);
     }
 }
---
+++ src/map.js
@@ -0,0 +1,12 @@
+const SELENGA_MAP = [
+    {
+        name: 'assets/inside23.png',
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+    {
+        name: 'assets/inside24.png',
+        position: [-0.06599999999999984, 0, -0.6320000000000006],
+        rotation: [-0.6539856454962627, 1.1624262485449337, 0.6130588426999719]
+    }
+];
