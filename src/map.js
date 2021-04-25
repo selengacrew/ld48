f4f759c7bfb84fffe1f85cbc3208333bbffc6a02
= <=>
2021-04-25 18:40:00 +0300

    map

--- src/map.js
+++ src/map.js
@@ -1,119 +1,146 @@
-SELENGA_MAP = {
-    'assets/inside1.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside10.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside12.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside14.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside15.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside17.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside19.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside2.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside20.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside21.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside22.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside23.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside24.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside25.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside26.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside27.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside28.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside30.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside31.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside32.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside33.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside34.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside4.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside41.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside43.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside44.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside5.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside6.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    'assets/inside9.png': {
-            position: [0, 0, 0],
-            rotation: [0, 0, 0]
-        },
-    };
-    
+const SELENGA_MAP =  {
+    "assets/inside1.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside10.png": {
+        position: [-0.27832895709763456, -0.6006785746410094, -0.7569498857545005],
+        rotation: [-2.2646333406944685, 1.1826988300035275, 2.302858685650188]
+    },
+
+    "assets/inside12.png": {
+        position: [-0.6891244552806856, -0.6299788191717965, -0.5394126775157053],
+        rotation: [-0.149272434798958, 1.025571787508338, 0.12788429008637958]
+    },
+
+    "assets/inside14.png": {
+        position: [-0.6034634777407493, -1.484785974175866, -0.49890922594859044],
+        rotation: [0.22684847487442378, 1.2508492837843523, -0.21569912414017986]
+    },
+
+    "assets/inside15.png": {
+        position: [-0.2543271457484877, -1.625026096017218, -0.5320275242591804],
+        rotation: [0.562416931998548, 0.8612401704723973, -0.7360512733199952]
+    },
+
+    "assets/inside17.png": {
+        position: [0.0738848145860019, -1.0442488168820914, -0.6137606749447482],
+        rotation: [0.28989134789245463, -0.7575186856650667, 0.20216435946024924]
+    },
+
+    "assets/inside19.png": {
+        position: [0.0738848145860019, -1.0442488168820914, -0.6137606749447482],
+        rotation: [-0.33990169829442224, 0.34787567332160607, 0.11997278779791833]
+    },
+
+    "assets/inside2.png": {
+        position: [0, 0, 0],
+        rotation: [0.055457520845636175, 0.22965871555452488, -0.012636924890073072]
+    },
+
+    "assets/inside20.png": {
+        position: [-0.1555354601712192, -0.5895319982148459, -1.3377654785395972],
+        rotation: [-0.45828163444682024, 0.3487409084726996, 0.16700141547840985]
+    },
+
+    "assets/inside21.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside22.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside23.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside24.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside25.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside26.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside27.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside28.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside30.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside31.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside32.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside33.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside34.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside4.png": {
+        position: [-0.018393511619837342, -0.2133134048213302, -0.3995768746190036],
+        rotation: [-0.5958824004750329, 0.07455216737217109, 0.050464790582898]
+    },
+
+    "assets/inside41.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside43.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside44.png": {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0]
+    },
+
+    "assets/inside5.png": {
+        position: [-0.1039619402977164, -0.31740367629861055, -0.6701647057092404],
+        rotation: [-0.6066830841117803, 0.04437737459448578, 0.03077776114699972]
+    },
+
+    "assets/inside6.png": {
+        position: [-0.1090514111552326, -0.6850740522652269, -0.8450017675514452],
+        rotation: [-1.3675233788996648, -1.3808390799479295, -1.36390443237699]
+    },
+
+    "assets/inside9.png": {
+        position: [0.46158319971307177, -0.4802071680608475, -0.7654602578851336],
+        rotation: [-2.5215237684686453, 1.4411372518916448, 2.525504659058619]
+    },
+};
