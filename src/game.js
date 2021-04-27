4b7f70eb0e2818c1685621d39b2954577e2356f7
algroznykh <groznykh.aleksandr@gmail.com>
2021-04-27 16:45:01 +0300

    maps

--- src/game.js
+++ src/game.js
@@ -67,7 +67,7 @@ function game_update(t, dt, state) {
         near_item.material.uniforms.angle_dist.value = camera_lookat.angleTo(near_lookat);
 
         near_item.material.uniforms.time.value = t;
-        if(item.distance > 0.5) {
+        if(item.distance > 0.5 ) {
             let x = item.distance * 2;
             near_item.scale.set(x, x, -x);
         } else {
@@ -158,6 +158,74 @@ function game_init(state) {
         void main() {
             vUv = uv;
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1);
+        }
+    `;
+
+    const editor_fragment = frag`
+
+        varying vec2 vUv;
+
+        uniform vec2 resolution;
+        uniform sampler2D texture0;
+        uniform float dist;
+        uniform float diff_dist;
+        uniform float angle_dist;
+        uniform float time;
+        uniform float opacity;
+
+        const mat3 sobelX = mat3(-1.0, 0.0, 1.0, -2.0, 0.0, 2.0, -1.0, 0.0, 1.0)/8.0;
+        const mat3 sobelY = mat3(-1.0,-2.0,-1.0, 0.0, 0.0, 0.0, 1.0, 2.0, 1.0)/8.0;
+        const mat3 gauss = mat3(1.0, 2.0, 1.0, 2.0, 4.0-16.0, 2.0, 1.0, 2.0, 1.0)/8.0;
+
+
+        vec3 conv3x3(vec2 uv, mat3 fil) {
+            vec3 a = vec3(0.0);
+            for (int y=0; y<3; ++y)
+            for (int x=0; x<3; ++x) {
+              vec2 p = uv * resolution + vec2(float(x-1), float(y-1));
+              a += fil[y][x] * texture2D(texture0, p / resolution).xyz;
+            }
+            return a;
+        }
+
+        void main() {
+            vec2 uv = vec2(1. - abs(vUv.x - 0.5) * 2., vUv.y);
+
+            vec4 origin_color = texture2D(texture0, uv);
+            
+            vec2 wooUv = uv * (1. + dist * 0.02 * sin(10. * time + sin(uv) * cos(uv) * 20.));
+            
+            vec3 sobel_color = (conv3x3(wooUv, sobelX) + conv3x3(wooUv, sobelY)) * 10.;
+
+            float fade = smoothstep(0.05, 0.5, dist);
+            float opacity_fade = smoothstep(0., 0.05, diff_dist) + 0.5;
+
+            vec3 frontcolor = mix(origin_color.xyz, sobel_color.xyz, fade + 0.15);
+            
+            float polar = smoothstep(0.05, 0.15, vUv.y) *
+            (1. - smoothstep(0.8, 0.9, vUv.y));
+            
+            vec3 backcolor = vec3(length(conv3x3(vec2(
+                fract(vUv.y + uv.x * sin(uv.x + sin(time * 2.4 + uv.y * 100.) * 0.2) * cos(vUv.x * 0.2 - cos(time * 1.9 + uv.x * 320.) * 0.3))
+            , uv.y), gauss))) * 20.;
+
+            float angle_fade = smoothstep(1.5, 2.6, angle_dist);
+
+            // smooth fade
+            backcolor *= smoothstep(0.01, 0.2, pow((vUv.x - 0.75) * 2., 2.) + pow(vUv.y - 0.5, 2.));
+            backcolor *= angle_fade;
+
+            float front = 
+                (1. - smoothstep(0.45, 0.52 , vUv.x)) * 
+                (smoothstep(0., 0.05, vUv.x)) *
+                polar *
+                (1. - angle_fade);
+
+            gl_FragColor = mix(
+                vec4(opacity_fade * backcolor, 0.5),
+                vec4(opacity_fade * frontcolor, origin_color.w),
+                front
+            ) * vec4(vec3(1.), opacity);
         }
     `;
     
@@ -302,7 +370,7 @@ function game_init(state) {
         const sphere_shader = new THREE.ShaderMaterial({
             uniforms: sphere_uniforms,
             vertexShader: sphere_vertex[0], //THREE.DefaultVertex,
-            fragmentShader: sphere_fragment[0],
+            fragmentShader: editor_fragment[0],
             side: THREE.DoubleSide,
             transparent: true,
             depthTest: false,
